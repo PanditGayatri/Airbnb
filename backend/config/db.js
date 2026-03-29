2515f5d4d7be8e0f65e507eac5a63d14f47cba7a
VyankateshPandit <ompandit1511@gmail.com>
2026-03-29 21:26:43 +0530

    log message changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB connected")
-    } catch (error) {
-        console.log("db error")
-    }
-}
-export default connectDb
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("DB connection error:", error.message);
+  }
+};
+export default connectDb;
